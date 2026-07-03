b0f6c98cd0640d5ecad263657a6d8e8951e98f3a
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-03 22:22:51 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/3/2026, 10:22:51 PM
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
+
+        // Mark rows and columns containing 0
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Set elements to 0 if their row or column is marked
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
