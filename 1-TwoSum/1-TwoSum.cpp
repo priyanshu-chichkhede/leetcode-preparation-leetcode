3cdd9e21eee03b88e6eb93de43f3a7e5cf3f3cc4
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-03 22:23:00 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/3/2026, 10:23:00 PM
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if(mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
