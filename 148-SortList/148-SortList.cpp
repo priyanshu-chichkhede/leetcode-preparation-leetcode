71321265e77c8f88c97d77de20a187ba256d2a83
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-03 22:22:43 +0530

    Create 148-SortList

---
+++ 148-SortList/148-SortList.cpp
@@ -0,0 +1,55 @@
+// Last updated: 7/3/2026, 10:22:43 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findMiddle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* left, ListNode* right) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (left != NULL && right != NULL) {
+            if (left->val <= right->val) {
+                tail->next = left;
+                left = left->next;
+            } else {
+                tail->next = right;
+                right = right->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = (left != NULL) ? left : right;
+        return dummy.next;
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+
+        ListNode* mid = findMiddle(head);
+        ListNode* rightHalf = mid->next;
+        mid->next = NULL;
+
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(rightHalf);
+
+        return merge(left, right);
+    }
+};
