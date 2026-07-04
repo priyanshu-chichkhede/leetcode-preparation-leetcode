14019981e107476c32c884af04eebb28520e26db
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-04 17:23:28 +0530

    File updated at 7/4/2026, 5:23:29 PM

--- 92-ReverseLinkedListII/92-ReverseLinkedListII.cpp
+++ 92-ReverseLinkedListII/92-ReverseLinkedListII.cpp
@@ -1,22 +1,25 @@
-// Last updated: 7/4/2026, 5:19:27 PM
+// Last updated: 7/4/2026, 5:23:29 PM
 1class Solution {
 2public:
 3    string removeDuplicates(string s) {
 4        int i = 0;
 5
 6        while (i + 1 < s.size()) {
-7            if (s[i] == s[i + 1]) {
-8                s.erase(i, 2);
-9
-10                if (i > 0) {
-11                    i--;
-12                }
-13            }
-14            else {
-15                i++;
+7            // the loop till the end is checked and then erase is done
+8            // erase(i,number of character we want to delete )
+9            //after deleting the 2 character next characters with number we have deleted comes
+10            if (s[i] == s[i + 1]) {
+11                s.erase(i, 2);
+12             // what if i<0 then it would be a runtime error to prevent that we keep i positive and if it is greater than 0 then we delete to make sure to check the previous number as well;  
+13                if (i > 0) {
+14                    i--;
+15                }
 16            }
-17        }
-18
-19        return s;
-20    }
-21};
+17            else {
+18                i++;
+19            }
+20        }
+21
+22        return s;
+23    }
+24};
