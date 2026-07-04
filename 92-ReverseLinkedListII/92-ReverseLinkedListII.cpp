d594bbede73dcff36d185452536671461667f3de
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-04 17:19:27 +0530

    File updated at 7/4/2026, 5:19:27 PM

--- 92-ReverseLinkedListII/92-ReverseLinkedListII.cpp
+++ 92-ReverseLinkedListII/92-ReverseLinkedListII.cpp
@@ -1,27 +1,22 @@
-// Last updated: 7/4/2026, 4:36:29 PM
+// Last updated: 7/4/2026, 5:19:27 PM
 1class Solution {
 2public:
-3    bool isPalindrome(string s) {
-4        int i=0;
-5        int n=s.length();
-6        int j=n-1;
-7        while(i<j){
-8            if(!isalnum(s[i])){
-9                i++;
-10                continue; //to discard all the next part of the loop and again come to this loop
-11            }
-12               if(!isalnum(s[j])){
-13                j--;
-14                continue;
-15            }
-16            if(tolower(s[i])!=tolower(s[j])){
-17                return false;
-18            }
-19            else{
-20                i++;
-21                j--;
-22            }
-23        }
-24        return true;
-25    }
-26};
+3    string removeDuplicates(string s) {
+4        int i = 0;
+5
+6        while (i + 1 < s.size()) {
+7            if (s[i] == s[i + 1]) {
+8                s.erase(i, 2);
+9
+10                if (i > 0) {
+11                    i--;
+12                }
+13            }
+14            else {
+15                i++;
+16            }
+17        }
+18
+19        return s;
+20    }
+21};
