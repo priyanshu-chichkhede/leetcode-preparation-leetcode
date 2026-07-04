f16fea4b3ffdc2bfd5c473c17aa48ca54d9d8b4d
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-04 14:53:36 +0530

    File updated at 7/4/2026, 2:53:36 PM

--- 697-DegreeofanArray/697-DegreeofanArray.cpp
+++ 697-DegreeofanArray/697-DegreeofanArray.cpp
@@ -1,27 +1,15 @@
-// Last updated: 7/4/2026, 2:40:10 PM
+// Last updated: 7/4/2026, 2:53:36 PM
 1class Solution {
 2public:
-3    string reverseVowels(string s) {
-4        int n = s.length();
-5        int i = 0;
-6        int j = n - 1;
-7
-8        while (i < j) {
-9            if (!(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U' ||
-10                  s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')) {
-11                i++;
-12            }
-13            else if (!(s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O' || s[j]=='U' ||
-14                       s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u')) {
-15                j--;
-16            }
-17            else {
-18                swap(s[i], s[j]);
-19                i++;
-20                j--;
-21            }
-22        }
-23
-24        return s;
-25    }
-26};
+3    void reverseString(vector<char>& s) {
+4        int i=0;
+5        int n=s.size();
+6        int j=n-1;
+7        while(i<j){
+8            swap(s[i],s[j]);
+9            i++;
+10            j--;
+11        }
+12        
+13    }
+14};
