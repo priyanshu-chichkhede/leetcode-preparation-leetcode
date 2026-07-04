37b18660e1af4b3962057726ef365e28a7d92a93
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-04 22:16:54 +0530

    File updated at 7/4/2026, 10:16:54 PM

--- 697-DegreeofanArray/697-DegreeofanArray.cpp
+++ 697-DegreeofanArray/697-DegreeofanArray.cpp
@@ -1,20 +1,36 @@
-// Last updated: 7/4/2026, 8:20:34 PM
+// Last updated: 7/4/2026, 10:16:54 PM
 1class Solution {
 2public:
-3    string removeOccurrences(string s, string part) {
-4        int i = 0;
-5        // string: : npos is present as not found part like there are two answer 
-6        //FOUND and npos which is given down 
-7        while (s.find(part) != string::npos) {
-8            i = s.find(part);
+3    bool checkInclusion(string s1, string s2) {
+4        int freq[26] = {0};
+5
+6        for (int i = 0; i < s1.length(); i++) {
+7            freq[s1[i] - 'a']++;
+8        }
 9
-10            s.erase(i, part.length());
+10        int windSize = s1.length();
 11
-12            if (i > 0) {
-13                i--;
-14            }
-15        }
-16
-17        return s;
-18    }
-19};
+12        for (int i = 0; i + windSize <= s2.length(); i++) {
+13            int windFreq[26] = {0};
+14
+15            for (int j = i; j < i + windSize; j++) {
+16                windFreq[s2[j] - 'a']++;
+17            }
+18
+19            bool same = true;
+20
+21            for (int j = 0; j < 26; j++) {
+22                if (freq[j] != windFreq[j]) {
+23                    same = false;
+24                    break;
+25                }
+26            }
+27
+28            if (same) {
+29                return true;
+30            }
+31        }
+32
+33        return false;
+34    }
+35};
