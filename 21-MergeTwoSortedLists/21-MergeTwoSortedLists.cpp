dd969483568a8dc4a8383b0d086e649ec68bbeb9
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-05 20:11:36 +0530

    File updated at 7/5/2026, 8:11:35 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/5/2026, 8:00:40 PM
+// Last updated: 7/5/2026, 8:11:35 PM
 1class Solution {
 2public:
 3    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
@@ -7,15 +7,19 @@
 6        vector<int> ans;
 7        for(int i=0;i<nums1.size();i++){
 8            mpp[nums1[i]]++;
-9        }
-10        for(int i=0;i<nums2.size();i++){
-11            if(mpp.find(nums2[i]) != mpp.end() && mpp[nums2[i]] > 0){
-12                ans.push_back(nums2[i]);
-13                mpp[nums2[i]]--;   
-14            }
-15            
-16        }
-17        return ans;
-18        
-19    }
-20};
+9            //here we check for count as we have duplicate values as well so we only 
+10            //decrease the count and check again as per 
+11        }
+12        for(int i=0;i<nums2.size();i++){
+13            if(mpp.find(nums2[i]) != mpp.end() && mpp[nums2[i]] > 0){
+14                ans.push_back(nums2[i]);
+15                mpp[nums2[i]]--;   
+16            }
+17            // if the count is 0 the map would still check it and send but if it is 0 then there is
+18            //no need of that as per so we check whether the count is greater than 0 as a must 
+19            //condition
+20        }
+21        return ans;
+22        
+23    }
+24};
