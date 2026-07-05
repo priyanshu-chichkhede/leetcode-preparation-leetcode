19aa2bdbaa509c456b04f76f99e5331dab0d89dc
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-05 14:26:27 +0530

    File updated at 7/5/2026, 2:26:26 PM

--- 344-ReverseString/344-ReverseString.cpp
+++ 344-ReverseString/344-ReverseString.cpp
@@ -1,15 +1,26 @@
-// Last updated: 7/4/2026, 4:42:15 PM
+// Last updated: 7/5/2026, 2:26:26 PM
 1class Solution {
 2public:
-3    void reverseString(vector<char>& s) {
-4        int i=0;
-5        int n=s.size();
-6        int j=n-1;
-7        while(i<j){
-8            swap(s[i],s[j]);
-9            i++;
-10            j--;
-11        }
-12        
-13    }
-14};
+3    bool isValid(string s) {
+4        int i = 0;
+5
+6        while(i + 1 < s.length()) {
+7
+8            if((s[i] == '(' && s[i+1] == ')') ||
+9               (s[i] == '{' && s[i+1] == '}') ||
+10               (s[i] == '[' && s[i+1] == ']')) {
+11
+12                s.erase(i, 2);
+13
+14                if(i > 0) {
+15                    i--;
+16                }
+17            }
+18            else {
+19                i++;
+20            }
+21        }
+22
+23        return s.empty();
+24    }
+25};
