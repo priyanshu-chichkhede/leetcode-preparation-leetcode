af96a711effd9cdb6fba87fc6da4a5d7f821bc1c
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-05 20:00:40 +0530

    File updated at 7/5/2026, 8:00:40 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -1,23 +1,21 @@
-// Last updated: 7/5/2026, 7:36:50 PM
+// Last updated: 7/5/2026, 8:00:40 PM
 1class Solution {
 2public:
-3    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-4
-5        unordered_map<int, int> mp;
+3    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+4        unordered_map<int,int> mpp;
+5        //use ordered map when u need keys and values in order
 6        vector<int> ans;
-7
-8        for(int i = 0; i < nums1.size(); i++) {
-9            mp[nums1[i]]++;
-10        }
-11
-12        for(int i = 0; i < nums2.size(); i++) {
-13
-14            if(mp.find(nums2[i]) != mp.end()) {
-15                ans.push_back(nums2[i]);
-16                mp.erase(nums2[i]);
-17            }
-18        }
-19
-20        return ans;
-21    }
-22};
+7        for(int i=0;i<nums1.size();i++){
+8            mpp[nums1[i]]++;
+9        }
+10        for(int i=0;i<nums2.size();i++){
+11            if(mpp.find(nums2[i]) != mpp.end() && mpp[nums2[i]] > 0){
+12                ans.push_back(nums2[i]);
+13                mpp[nums2[i]]--;   
+14            }
+15            
+16        }
+17        return ans;
+18        
+19    }
+20};
