848787865a9eb88123a2be16dc6b726cb5370804
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-09 22:32:46 +0530

    File updated at 7/9/2026, 10:32:44 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.cpp
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -1,43 +1,16 @@
-// Last updated: 7/3/2026, 10:22:59 PM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-
-        int carry = 0;
-
-        while (l1 || l2 || carry) {
-
-            int sum = carry;
-
-            if (l1) {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-
-            if (l2) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
-
-            carry = sum / 10;
-
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
-        }
-
-        return dummy->next;
-    }
-};
+// Last updated: 7/9/2026, 10:32:44 PM
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        int n=nums.size();
+5        for(int i=0;i<n;i++){
+6            
+7            for(int j=i+1;j<n;j++){
+8                if(nums[i]+nums[j]==target){
+9                    return {i,j};
+10                }
+11            }
+12        }
+13        return {};
+14    }
+15};
