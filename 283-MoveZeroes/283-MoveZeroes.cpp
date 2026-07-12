fe938ce30893ad57e38987ca9633a07c08e2ab9b
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-13 00:49:19 +0530

    File updated at 7/13/2026, 12:49:18 AM

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,17 +1,25 @@
-// Last updated: 7/13/2026, 12:18:40 AM
+// Last updated: 7/13/2026, 12:49:18 AM
 1class Solution {
 2public:
-3    void moveZeroes(vector<int>& nums) {
-4        int n=nums.size();
-5            int i=0;
-6            for(int j=0;j<n;j++){
-7                if(nums[j]!=0){
-8                    swap(nums[i],nums[j]);
-9                    i++;
-10                }
-11                
-12               
-13            }
-14        
-15    }
-16};
+3    vector<int> concatWithReverse(vector<int>& nums) {
+4        vector <int> nums2;
+5        for(int i=0;i<nums.size();i++){
+6         nums2.push_back(nums[i]);
+7        }
+8        int j=0;
+9        int k=nums2.size()-1;
+10        while(j<k){
+11           swap(nums2[k],nums2[j]); 
+12           j++;
+13           k--;
+14        }
+15        vector<int> ans;
+16        for(int i=0;i<nums.size();i++){
+17            ans.push_back(nums[i]);
+18        }
+19        for(int i=0;i<nums2.size();i++){
+20            ans.push_back(nums2[i]);
+21        }
+22        return ans;
+23    }
+24};
