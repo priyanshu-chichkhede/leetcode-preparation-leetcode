bcd06679ecd6dd91c9642d1438a7cafd15b4fa70
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-12 23:12:39 +0530

    File updated at 7/12/2026, 11:12:38 PM

--- 18-4Sum/18-4Sum.cpp
+++ 18-4Sum/18-4Sum.cpp
@@ -1,32 +1,25 @@
-// Last updated: 7/12/2026, 8:57:48 PM
+// Last updated: 7/12/2026, 11:12:38 PM
 1class Solution {
 2public:
-3    int compress(vector<char>& chars) {
-4        int n=chars.size();
-5        int idx=0;
-6        for(int i=0;i<n;i++){
-7            int count=1;//because the current count is always taken with one letter 
-8            //as aaa if count is 0 then we get 2 count but there are 3 so we put 1 because
-9            //we count the one common occurrence as well
-10           while(i<n-1 && chars[i]==chars[i+1]){
-11            count++;
-12            i++;
-13           }
-14           if(count==1){
-15            chars[idx]=chars[i];
-16            idx++;
-17           }
-18           else{
-19              chars[idx]=chars[i];
-20                idx++;
-21            string str=to_string(count);
-22            for(int j=0;j<str.length();j++){
-23              chars[idx]=str[j];
-24              idx++;
-25            }
-26            
-27           }
-28        }
-29        return idx;
-30    }
-31};
+3    int romanToInt(string s) {
+4        unordered_map<char,int> mpp;
+5    mpp['I'] = 1;              
+6    mpp['V'] = 5;
+7    mpp['X'] = 10;
+8    mpp['L'] = 50;
+9    mpp['C'] = 100;
+10    mpp['D'] = 500;
+11    mpp['M'] = 1000;
+12    int ans=0; 
+13    for(int i=0;i<s.length();i++){
+14        if(mpp[s[i]]<mpp[s[i+1]]){
+15            ans=ans-mpp[s[i]];
+16        }
+17        else{
+18            ans=ans+mpp[s[i]];
+19        }
+20    }
+21    return ans;
+22
+23    }
+24};
