f2bcfd195a39c71fd323f322e3c6b00df694159e
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-12 23:45:19 +0530

    File updated at 7/12/2026, 11:45:18 PM

--- 18-4Sum/18-4Sum.cpp
+++ 18-4Sum/18-4Sum.cpp
@@ -1,19 +1,22 @@
-// Last updated: 7/12/2026, 11:28:04 PM
+// Last updated: 7/12/2026, 11:45:18 PM
 1class Solution {
 2public:
-3    bool isPalindrome(int x) {
-4        string str=to_string(x);
-5        int i=0;
-6        int j=str.length()-1;
-7        while(i<j){
-8            if(str[i]==str[j]){
-9            i++;
-10            j--;
-11            
-12        }
-13        else{
-14            return false;
-15        }}
-16        return true;
-17    }
-18};
+3    bool checkPerfectNumber(int num) {
+4        vector <int> ans;
+5        for(int i=1;i<num;i++){
+6            if(num%i==0){
+7                ans.push_back(i);
+8            }
+9        }
+10        int sum=0;
+11        for(int j=0;j<ans.size();j++){
+12            sum=sum+ans[j];
+13        }
+14        if(sum==num){
+15            return true;
+16        }
+17        else{
+18            return false;
+19        }
+20    }
+21};
