83271dd39c7163519a1ec5a7deb3a383364a8c29
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-07-23 02:03:11 +0530

    File updated at 7/23/2026, 2:03:10 AM

--- 796-RotateString/796-RotateString.cpp
+++ 796-RotateString/796-RotateString.cpp
@@ -1,23 +1,21 @@
-// Last updated: 7/23/2026, 1:42:51 AM
+// Last updated: 7/23/2026, 2:03:10 AM
 1class Solution {
 2public:
-3    int secondHighest(string s) {
-4        int n=s.length();
-5        vector<int> ans;
+3    int diagonalSum(vector<vector<int>>& mat) {
+4        int n=mat.size();
+5        int sum=0;
 6        for(int i=0;i<n;i++){
-7           if(s[i]>='0' and s[i]<='9'  ){
-8            int num=s[i]-'0';
-9            ans.push_back(num);
-10           } 
-11
-12        }
-13        sort(ans.begin(),ans.end());
-14    for (int i = ans.size() - 2; i >= 0; i--) {
-15            if (ans[i] != ans.back()) {
-16                return ans[i];
-17            }
-18        }
-19
-20        return -1;
-21    }
-22};
+7            for(int j=0;j<n;j++){
+8                if(i==j){
+9                    sum=sum+mat[i][j];
+10                }
+11                else if(i+j==n-1){
+12                    sum+=mat[i][j];
+13                }
+14
+15                
+16            }
+17        }
+18        return sum;
+19    }
+20};
