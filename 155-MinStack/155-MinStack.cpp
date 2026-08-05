f3a2ded5d063bb3c2da6ff4b0761cb2b0cbfa317
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-08-05 22:50:42 +0530

    File updated at 8/5/2026, 10:50:42 PM

--- 155-MinStack/155-MinStack.cpp
+++ 155-MinStack/155-MinStack.cpp
@@ -1,48 +1,29 @@
-// Last updated: 8/5/2026, 10:18:42 PM
+// Last updated: 8/5/2026, 10:50:42 PM
 1class Solution {
 2public:
-3    int largestRectangleArea(vector<int>& heights) {
-4        int n=heights.size();
-5        vector<int> left;
-6        vector<int> right;
-7        stack<int> st;
-8        for(int i=0;i<n;i++){
-9            while(st.size()>0 && heights[st.top()]>=heights[i]){
-10                st.pop();
-11            }
-12            if(st.size()==0){
-13                left.push_back(-1);
-14            }
-15            else{
-16                left.push_back(st.top());
-17            }
-18            st.push(i);
-19        }
-20        while(!st.empty()){
-21            st.pop();
-22        }
-23        for(int i=n-1;i>=0;i--){
-24            while(st.size()>0 && heights[st.top()]>=heights[i]){
-25                st.pop();
-26            }
-27            if(st.size()==0){
-28                right.push_back(n);
-29            }
-30            else{
-31                right.push_back(st.top());
-32            }
-33            
-34            st.push(i);
-35        }
-36        reverse(right.begin(), right.end());
-37        int ans=0;
-38        for(int i=0;i<n;i++){
-39            int width=right[i]-left[i]-1;
-40            int area=heights[i]*width;
-41             ans=max(ans,area);
-42        }
-43        return ans;
-44
-45
-46    }
-47};
+3    vector<int> nextGreaterElements(vector<int>& nums) {
+4        vector<int> ans;
+5        vector<int> main;
+6        int n=nums.size();
+7        for(int i=0;i<n;i++){
+8            ans.push_back(nums[i]);
+9        }
+10        for(int i=0;i<n;i++){
+11            ans.push_back(nums[i]);
+12        }
+13        for(int i=0;i<ans.size()/2;i++){
+14            
+15            for(int j=i+1;j<ans.size();j++){
+16                if(ans[j]>ans[i]){
+17                    main.push_back(ans[j]);
+18                    break;
+19                }
+20                if(j==ans.size()-1){
+21                    main.push_back(-1);
+22                }
+23               
+24            }
+25        }
+26        return main;
+27    }
+28};
