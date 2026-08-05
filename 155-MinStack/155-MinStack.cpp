52e61e536f3bd16483e313142597c2b53780747e
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-08-05 23:26:25 +0530

    File updated at 8/5/2026, 11:26:25 PM

--- 155-MinStack/155-MinStack.cpp
+++ 155-MinStack/155-MinStack.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/5/2026, 11:21:49 PM
+// Last updated: 8/5/2026, 11:26:25 PM
 1class Solution {
 2public:
 3    vector<int> nextGreaterElements(vector<int>& nums) {
@@ -26,24 +26,43 @@
 25        //     }
 26        // }
 27        // return main;
-28        int n=nums.size();
-29        vector<int> ans;
-30        stack<int> st;
-31        for(int i= 2*n-1;i>=0;i--){
-32            while(st.size()>0 && nums[st.top()]<=nums[i%n]){
-33                st.pop();
-34            }
-35        if (i < n) {
-36                if (st.empty()) {
-37                    ans.push_back(-1);
-38                } else {
-39                    ans.push_back(nums[st.top()]);
-40                }
-41            }
-42            st.push(i%n);
+28        // int n=nums.size();
+29        // vector<int> ans;
+30        // stack<int> st;
+31        // for(int i= 2*n-1;i>=0;i--){
+32        //     while(st.size()>0 && nums[st.top()]<=nums[i%n]){
+33        //         st.pop();
+34        //     }
+35        // if (i < n)//prepare the stack till the actual like array not the duplicate array comes  {
+36        //         if (st.empty()) {
+37        //             ans.push_back(-1);
+38        //         } else {
+39        //             ans.push_back(nums[st.top()]);
+40        //         }
+41        //     }
+42        //     st.push(i%n);
 43
-44        }
-45        reverse(ans.begin(), ans.end());
-46        return ans;
-47    }
-48};
+44        // }
+45        // reverse(ans.begin(), ans.end());
+46        // return ans;
+47
+48        int n = nums.size();
+49        vector<int> ans(n, -1);
+50        stack<int> st;
+51
+52        for (int i = 2 * n - 1; i >= 0; i--) {
+53
+54            while (!st.empty() && nums[st.top()] <= nums[i % n]) {
+55                st.pop();
+56            }
+57
+58            if (!st.empty()) {
+59                ans[i % n] = nums[st.top()];
+60            }
+61
+62            st.push(i % n);
+63        }
+64
+65        return ans;
+66    }
+67};
