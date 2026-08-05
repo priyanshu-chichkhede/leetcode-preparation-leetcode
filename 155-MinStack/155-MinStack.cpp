7784bae73a19733492af07ace812876d3da3d4b4
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-08-05 23:21:49 +0530

    File updated at 8/5/2026, 11:21:49 PM

--- 155-MinStack/155-MinStack.cpp
+++ 155-MinStack/155-MinStack.cpp
@@ -1,29 +1,49 @@
-// Last updated: 8/5/2026, 10:50:42 PM
+// Last updated: 8/5/2026, 11:21:49 PM
 1class Solution {
 2public:
 3    vector<int> nextGreaterElements(vector<int>& nums) {
-4        vector<int> ans;
-5        vector<int> main;
-6        int n=nums.size();
-7        for(int i=0;i<n;i++){
-8            ans.push_back(nums[i]);
-9        }
-10        for(int i=0;i<n;i++){
-11            ans.push_back(nums[i]);
-12        }
-13        for(int i=0;i<ans.size()/2;i++){
-14            
-15            for(int j=i+1;j<ans.size();j++){
-16                if(ans[j]>ans[i]){
-17                    main.push_back(ans[j]);
-18                    break;
-19                }
-20                if(j==ans.size()-1){
-21                    main.push_back(-1);
-22                }
-23               
-24            }
-25        }
-26        return main;
-27    }
-28};
+4        //BRUTE FORCE APPROACH
+5        // vector<int> ans;
+6        // vector<int> main;
+7        // int n=nums.size();
+8        // for(int i=0;i<n;i++){
+9        //     ans.push_back(nums[i]);
+10        // }
+11        // for(int i=0;i<n;i++){
+12        //     ans.push_back(nums[i]);
+13        // }
+14        // for(int i=0;i<ans.size()/2;i++){
+15            
+16        //     for(int j=i+1;j<ans.size();j++){
+17        //         if(ans[j]>ans[i]){
+18        //             main.push_back(ans[j]);
+19        //             break;
+20        //         }
+21        //         if(j==ans.size()-1){
+22        //             main.push_back(-1);
+23        //         }
+24               
+25        //     }
+26        // }
+27        // return main;
+28        int n=nums.size();
+29        vector<int> ans;
+30        stack<int> st;
+31        for(int i= 2*n-1;i>=0;i--){
+32            while(st.size()>0 && nums[st.top()]<=nums[i%n]){
+33                st.pop();
+34            }
+35        if (i < n) {
+36                if (st.empty()) {
+37                    ans.push_back(-1);
+38                } else {
+39                    ans.push_back(nums[st.top()]);
+40                }
+41            }
+42            st.push(i%n);
+43
+44        }
+45        reverse(ans.begin(), ans.end());
+46        return ans;
+47    }
+48};
