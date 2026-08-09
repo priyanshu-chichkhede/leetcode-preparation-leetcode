04c162e153db2db850e9ba26c0b356969e7e94a7
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-08-09 15:44:49 +0530

    File updated at 8/9/2026, 3:44:49 PM

--- 100-SameTree/100-SameTree.cpp
+++ 100-SameTree/100-SameTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/9/2026, 1:37:35 AM
+// Last updated: 8/9/2026, 3:44:49 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -12,12 +12,24 @@
 11 */
 12class Solution {
 13public:
-14    bool isSameTree(TreeNode* p, TreeNode* q) {
+14    bool isIdentical(TreeNode* p, TreeNode* q){
 15        if(p==NULL || q==NULL){
 16            return p==q;
-17        }
-18      bool sameLeft=isSameTree(p->left, q->left);
-19      bool sameRight=isSameTree(p->right, q->right);  
-20      return sameLeft and sameRight and p->val==q->val;
+17
+18        }
+19        return p->val==q->val && isIdentical( p->left,q->left)
+20            and isIdentical( p->right, q->right);  
 21    }
-22};
+22    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+23        //base case
+24        if(root==NULL || subRoot==NULL){
+25            return root==subRoot;
+26            //same if else condition where we just check if
+27            //equal then true and not equal then not true
+28        }
+29        if(root->val==subRoot->val and isIdentical(root,subRoot) ){
+30            return true;
+31        }
+32        return isSubtree(root->left,subRoot)|| isSubtree(root->right,subRoot);
+33    } 
+34};
