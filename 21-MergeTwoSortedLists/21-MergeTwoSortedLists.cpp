febcd03fe94523a1ab1a094e2b6170063afa0784
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-08-17 01:48:44 +0530

    File updated at 8/17/2026, 1:48:43 AM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -1,28 +1,13 @@
-// Last updated: 8/17/2026, 1:37:58 AM
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution {
-12public:
-13    ListNode* deleteDuplicates(ListNode* head) {
-14        ListNode* curr = head;
-15
-16        while (curr != NULL && curr->next != NULL) {
-17            if (curr->val == curr->next->val) {
-18                curr->next = curr->next->next;
-19            }
-20            else {
-21                curr = curr->next;
-22            }
-23        }
-24
-25        return head;
-26    }
-27};
+// Last updated: 8/17/2026, 1:48:43 AM
+1class Solution {
+2public:
+3    int singleNumber(vector<int>& nums) {
+4        int ans = 0;
+5
+6        for(int i = 0; i < nums.size(); i++) {
+7            ans = ans^nums[i];
+8        }
+9
+10        return ans;
+11    }
+12};
