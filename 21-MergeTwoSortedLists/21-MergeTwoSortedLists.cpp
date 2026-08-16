208ec486a62c54954f9725b184408496e42c360d
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-08-17 01:37:58 +0530

    File updated at 8/17/2026, 1:37:58 AM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -1,25 +1,28 @@
-// Last updated: 7/5/2026, 8:11:35 PM
-1class Solution {
-2public:
-3    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-4        unordered_map<int,int> mpp;
-5        //use ordered map when u need keys and values in order
-6        vector<int> ans;
-7        for(int i=0;i<nums1.size();i++){
-8            mpp[nums1[i]]++;
-9            //here we check for count as we have duplicate values as well so we only 
-10            //decrease the count and check again as per 
-11        }
-12        for(int i=0;i<nums2.size();i++){
-13            if(mpp.find(nums2[i]) != mpp.end() && mpp[nums2[i]] > 0){
-14                ans.push_back(nums2[i]);
-15                mpp[nums2[i]]--;   
-16            }
-17            // if the count is 0 the map would still check it and send but if it is 0 then there is
-18            //no need of that as per so we check whether the count is greater than 0 as a must 
-19            //condition
-20        }
-21        return ans;
-22        
-23    }
-24};
+// Last updated: 8/17/2026, 1:37:58 AM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* deleteDuplicates(ListNode* head) {
+14        ListNode* curr = head;
+15
+16        while (curr != NULL && curr->next != NULL) {
+17            if (curr->val == curr->next->val) {
+18                curr->next = curr->next->next;
+19            }
+20            else {
+21                curr = curr->next;
+22            }
+23        }
+24
+25        return head;
+26    }
+27};
