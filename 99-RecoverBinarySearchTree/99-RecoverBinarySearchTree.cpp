9af747ab6081065293ebecbb01463d626496400e
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-08-19 16:26:46 +0530

    File updated at 8/19/2026, 4:26:46 PM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.cpp
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.cpp
@@ -1,40 +1,30 @@
-// Last updated: 8/19/2026, 2:42:29 PM
-1/**
-2 * Definition for a binary tree node.
-3 * struct TreeNode {
-4 *     int val;
-5 *     TreeNode *left;
-6 *     TreeNode *right;
-7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13public:
-14    TreeNode* prev=NULL;
-15    TreeNode* first=NULL;
-16    TreeNode* second=NULL;
-17    void inorder(TreeNode* root){
-18        if(root==NULL){
-19            return;
-20        }
-21        inorder(root->left);
-22        if(prev!=NULL and prev->val>root->val){
-23            if(first==NULL){
-24                first=prev;
-25            }
-26            second=root;
-27        }
-28        prev=root;//iterating forward
-29        inorder(root->right);
-30
-31
-32    }
-33    void recoverTree(TreeNode* root) {
-34        inorder(root);
-35            int temp=first->val;
-36            first->val=second->val;
-37            second->val=temp;
-38    }
-39};
+// Last updated: 8/19/2026, 4:26:46 PM
+1class Solution {
+2public:
+3    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+4        
+5        int n = grid.size();
+6
+7        vector<vector<int>> ans(
+8            n - 2,
+9            vector<int>(n - 2)
+10        );
+11
+12        for (int i = 0; i < n - 2; i++) {
+13            for (int j = 0; j < n - 2; j++) {
+14
+15                int maxi = 0;
+16
+17                for (int a = i; a < i + 3; a++) {
+18                    for (int b = j; b < j + 3; b++) {
+19                        maxi = max(maxi, grid[a][b]);
+20                    }
+21                }
+22
+23                ans[i][j] = maxi;
+24            }
+25        }
+26
+27        return ans;
+28    }
+29};
