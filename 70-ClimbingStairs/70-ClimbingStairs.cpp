3d81ba48399bdfad15f3337b23fd46b86d1f0f35
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-09-12 14:41:21 +0530

    File updated at 9/12/2026, 2:41:22 PM

--- 70-ClimbingStairs/70-ClimbingStairs.cpp
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -1,19 +1,22 @@
-// Last updated: 9/12/2026, 1:29:12 PM
+// Last updated: 9/12/2026, 2:41:22 PM
 1class Solution {
 2public:
-3  int climbStairs(int n) {
-4    if (n <= 2)
-5        return n;
-6
-7    vector<int> dp(n + 1);
-8
-9    dp[1] = 1;
-10    dp[2] = 2;
+3    int helper(int n, vector<int>& ans) {
+4        if (n <= 2) {
+5            return n;
+6        }
+7
+8        if (ans[n] != -1) {
+9            return ans[n];
+10        }
 11
-12    for (int i = 3; i <= n; i++) {
-13        dp[i] = dp[i-1] + dp[i-2];
-14    }
-15
-16    return dp[n];
-17}
-18};
+12        ans[n] = helper(n-1, ans) + helper(n-2, ans);
+13
+14        return ans[n];
+15    }
+16
+17    int climbStairs(int n) {
+18        vector<int> ans(n+1, -1);
+19        return helper(n, ans);
+20    }
+21};
