c03dde63ceaa52a75da5fc2ad6fcad36ed417a21
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-09-14 23:10:41 +0530

    File updated at 9/14/2026, 11:10:41 PM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.cpp
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.cpp
@@ -1,14 +1,22 @@
-// Last updated: 9/14/2026, 10:55:09 PM
+// Last updated: 9/14/2026, 11:10:41 PM
 1class Solution {
 2public:
-3    int minCostClimbingStairs(vector<int>& cost) {
-4        int n=cost.size();
-5        vector<int> dp(n);
-6        dp[0]=cost[0];
-7        dp[1]=cost[1];
-8        for(int i=2;i<n;i++){
-9            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+3    int numberOfSteps(int num) {
+4        int count=0;
+5        while(num!=0){
+6        if(num%2==0){
+7            num=num/2;
+8            count++;
+9            continue;
 10        }
-11        return min(dp[n-1],dp[n-2]);
-12    }
-13};
+11        else{
+12            num=num-1;
+13            count++;
+14            continue;
+15        }}
+16        return count;
+17        
+18            
+19        
+20    }
+21};
