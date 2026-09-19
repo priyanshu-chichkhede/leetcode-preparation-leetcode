b93b380f58f5fa04fd3fd336c8db5eb0b39d8d8c
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-09-19 16:48:40 +0530

    File updated at 9/19/2026, 4:48:39 PM

--- 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.cpp
+++ 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.cpp
@@ -1,25 +1,24 @@
-// Last updated: 9/19/2026, 3:03:50 PM
+// Last updated: 9/19/2026, 4:48:39 PM
 1class Solution {
 2public:
-3    int maxProduct(vector<int>& nums) {
-4        int n = nums.size();
-5
-6        int maxi = nums[0];
-7        int mini = nums[0];
-8        int ans = nums[0];
-9
-10        for(int i = 1; i < n; i++) {
-11
-12            int a = nums[i];
-13            int b = maxi * nums[i];
-14            int c = mini * nums[i];
-15
-16            maxi = max(a, max(b, c));
-17            mini = min(a, min(b, c));
-18
-19            ans = max(ans, maxi);
-20        }
-21
-22        return ans;
-23    }
-24};
+3    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+4        int n = fruits.size();
+5        int m = baskets.size();
+6        int count = 0;
+7
+8        for(int i = 0; i < n; i++) {
+9            for(int j = 0; j < m; j++) {
+10                if(fruits[i] <= baskets[j]) {
+11                    baskets[j] = -1;
+12                    break;
+13                }
+14
+15                if(j == m - 1) {
+16                    count++;
+17                }
+18            }
+19        }
+20
+21        return count;
+22    }
+23};
