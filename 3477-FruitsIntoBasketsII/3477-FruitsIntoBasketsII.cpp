b13b13f2cc0c14abff7c7ec7e2ddba197d21955b
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-09-19 13:25:41 +0530

    File updated at 9/19/2026, 1:25:39 PM

--- 3477-FruitsIntoBasketsII/3477-FruitsIntoBasketsII.cpp
+++ 3477-FruitsIntoBasketsII/3477-FruitsIntoBasketsII.cpp
@@ -1,4 +1,4 @@
-// Last updated: 9/19/2026, 1:22:07 PM
+// Last updated: 9/19/2026, 1:25:39 PM
 1class Solution {
 2public:
 3    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
@@ -7,20 +7,18 @@
 6        int count = 0;
 7
 8        for(int i = 0; i < n; i++) {
-9            int j;
-10
-11            for(j = 0; j < m; j++) {
-12                if(fruits[i] <= baskets[j]) {
-13                    baskets[j] = -1;
-14                    break;
-15                }
-16            }
-17
-18            if(j == m) {
-19                count++;
-20            }
-21        }
-22
-23        return count;
-24    }
-25};
+9            for(int j = 0; j < m; j++) {
+10                if(fruits[i] <= baskets[j]) {
+11                    baskets[j] = -1;
+12                    break;
+13                }
+14
+15                if(j == m - 1) {
+16                    count++;
+17                }
+18            }
+19        }
+20
+21        return count;
+22    }
+23};
