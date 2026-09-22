0c3e97b3f1bc4d0d1c9e5d94fe4bc6e9f1a45d19
Priyanshu Chichkhede <priyanshuchichkhede25139@gmail.com>
2026-09-22 13:21:30 +0530

    File updated at 9/22/2026, 1:21:29 PM

--- 3477-FruitsIntoBasketsII/3477-FruitsIntoBasketsII.cpp
+++ 3477-FruitsIntoBasketsII/3477-FruitsIntoBasketsII.cpp
@@ -1,24 +1,20 @@
-// Last updated: 9/19/2026, 1:25:39 PM
+// Last updated: 9/22/2026, 1:21:29 PM
 1class Solution {
 2public:
-3    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-4        int n = fruits.size();
-5        int m = baskets.size();
-6        int count = 0;
-7
-8        for(int i = 0; i < n; i++) {
-9            for(int j = 0; j < m; j++) {
-10                if(fruits[i] <= baskets[j]) {
-11                    baskets[j] = -1;
-12                    break;
-13                }
-14
-15                if(j == m - 1) {
-16                    count++;
-17                }
-18            }
-19        }
-20
-21        return count;
-22    }
-23};
+3    int maxAscendingSum(vector<int>& nums) {
+4        int n=nums.size();
+5        int sum=nums[0];
+6        int maxSum=nums[0];
+7        for(int i=1;i<n;i++){
+8            if(nums[i]>nums[i-1]){
+9                sum+=nums[i];
+10            }
+11            else{
+12                sum=nums[i];
+13            }
+14            maxSum=max(maxSum,sum);
+15        }
+16     
+17        return maxSum;
+18    }
+19};
